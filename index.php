0ccb90b2b1d1d2b353baa649ae12ceaaf9061537
Artem <154737256+CodeCrafter-dotcom@users.noreply.github.com>
2025-07-20 20:07:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
    $userAgent = $_SERVER['HTTP_USER_AGENT'];
 
    if (preg_match('/Mobile|Android|iPhone/', $userAgent)) {
-       header('Location: http://m.example.com');
+       header('Location: https://codecrafter-dotcom.github.io/McLaren/');
        exit();
    } else {
        header('Location: https://codecrafter-dotcom.github.io/FromBoard-Delivery/');
